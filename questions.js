8c5ce1480d12acd77ec36af721a51fd4e366547e
RASSixers <rashappy9@gmail.com>
2025-10-25 18:17:47 -0400

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,34 @@
+window.QUESTIONS = [
+  {
+    question: "Will the Sixers win the championship this season?",
+    type: "yn"
+  },
+  {
+    question: "How many games will the Sixers win this season?",
+    type: "numeric"
+  },
+  {
+    question: "Who will be the leading scorer for the Sixers this season?",
+    type: "mc",
+    options: [
+      { label: "Jalen Embiid", value: "embiid" },
+      { label: "Tyrese Maxey", value: "maxey" },
+      { label: "Paul George", value: "george" },
+      { label: "Other", value: "other" }
+    ]
+  },
+  {
+    question: "Will the Sixers make the Eastern Conference Finals?",
+    type: "yn"
+  },
+  {
+    question: "Best playoff matchup for the Sixers?",
+    type: "mc",
+    options: [
+      { label: "vs Boston Celtics", value: "celtics" },
+      { label: "vs Miami Heat", value: "heat" },
+      { label: "vs New York Knicks", value: "knicks" },
+      { label: "vs Milwaukee Bucks", value: "bucks" }
+    ]
+  }
+];
